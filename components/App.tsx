b9277dec644d1e3b6020039c9e2982e2545591b3
Luis Pedraza <luispedraza@utexas.edu>
2020-03-02 12:59:02 +1100

    reorganized styling

--- components/App.tsx
+++ components/App.tsx
@@ -148,10 +148,6 @@ const App: FunctionComponent = () => {
           margin-right: 10px;
         }
 
-        .hide {
-          display: none;
-        }
-
         .title {
           margin-top: 0;
           margin-bottom: 0;
@@ -168,6 +164,12 @@ const App: FunctionComponent = () => {
           font-weight: 400;
         }
 
+        .container {
+          margin-top: 15px;
+          display: flex;
+          height: 78%;
+        }
+
         .left-side {
           width: 80%;
           min-width: 120px;
@@ -178,19 +180,17 @@ const App: FunctionComponent = () => {
           display: flex;
         }
 
-        .graph-info {
-          width: 80%;
-        }
-
-        .container {
-          margin-top: 15px;
+        .right-side {
+          min-width: 100px;
+          width: 20%;
+          margin-right: 20px;
           display: flex;
-          height: 78%;
         }
 
         /* Graph Container */
 
         .graph-info {
+          width: 80%;
           display: flex;
           flex-direction: column;
           justify-content: flex-start;
@@ -204,191 +204,9 @@ const App: FunctionComponent = () => {
           height: 100%;
         }
 
-        .default-text {
-          margin: 18px;
-          font-size: 1.25em;
-          color: #747474;
-          font-weight: 700;
-        }
-
-        .default-text a {
-          text-decoration: underline;
-          color: #747474;
-        }
-
-        .default-text a:visited {
-          text-decoration: none;
-          color: #747474;
-        }
-
-        .default-text a:hover {
-          color: black;
-        }
-
-        .info-text {
-          max-width: 960px;
-          margin: 0;
-          font-size: 1.25em;
-          color: #747474;
-          font-weight: 700;
-        }
-
-        .info-name {
-          font-size: 2.5em;
-          color: #e10000;
-          font-weight: 400;
-          padding-right: 5px;
-        }
-
-        .CountryList {
-          list-style: none;
-          min-width: 120px;
-          max-width: 200px;
-          width: 15%;
-          overflow: scroll;
-          font-size: 1.5em;
-        }
-
-        .CountryList-links {
-          list-style-type: none;
-          margin: 0;
-          padding: 0;
-        }
-
-        .location-button {
-          text-align: left;
-          background-color: #ffffff;
-          border: none;
-          outline: none;
-          cursor: pointer;
-          font-weight: 700;
-          font-size: 0.75em;
-        }
-
-        .location-button:hover {
-          color: #747474;
-        }
-
-        .location-selected {
-          background-color: #ffffff;
-          text-align: left;
-          border: none;
-          outline: none;
-          cursor: pointer;
-          font-weight: 700;
-          font-size: 0.75em;
-          color: #e10000;
-        }
-
-        .info-text {
-          margin-left: 20px;
-        }
-
         .domain {
           fill: none;
           stroke: none;
-        }
-
-        /* Journalist Container */
-
-        .right-side {
-          min-width: 100px;
-          width: 20%;
-          margin-right: 20px;
-          display: flex;
-        }
-
-        .names {
-          padding-right: 15px;
-          overflow: scroll;
-        }
-
-        .name-section {
-        }
-
-        .names-year {
-          margin: 0;
-          flex-shrink: 0;
-          flex-grow: 0;
-          display: inline;
-          font-size: 3.33em;
-        }
-
-        .name-button {
-          background-color: #ffffff;
-          display: flex;
-          align-items: left;
-          text-align: left;
-          justify: left;
-          color: #747474;
-          padding: 3px;
-          border: none;
-          outline: none;
-          cursor: pointer;
-          font-size: 1.5em;
-        }
-
-        .name-button:hover {
-          color: #e10000;
-        }
-
-        /* Journalist Pane */
-        .journalist-container {
-          overflow: scroll;
-        }
-
-        .journalistPane {
-          min-width: 100px;
-          right: none;
-          text-align: justify;
-        }
-
-        .journalistPane p {
-          margin: 5px;
-        }
-
-        .closePaneButton {
-          margin-top: 15px;
-          margin-bottom: 25px;
-          background-color: #ffffff;
-          /* margin-left: -7px; */
-          border: none;
-          outline: none;
-          font-size: 1.5em;
-          cursor: pointer;
-        }
-
-        .closePaneButton:hover {
-          color: #e10000;
-        }
-
-        .journalist-name {
-          font-size: 3em;
-          text-align: left;
-          font-weight: 700;
-        }
-
-        .journalist-date {
-          padding-top: 20px;
-          font-size: 2em;
-          text-align: left;
-        }
-
-        .journalist-location {
-          font-size: 2em;
-          text-align: left;
-        }
-
-        .journalist-organization {
-          padding-top: 20px;
-          font-size: 2em;
-          text-align: left;
-        }
-
-        .description {
-          padding-top: 20px;
-          padding-right: 15px;
-          text-justify: inter-word;
         }
 
         @media (max-width: 700px) {
@@ -419,54 +237,8 @@ const App: FunctionComponent = () => {
             flex-direction: column;
           }
 
-          .CountryList {
-            max-width: 100%;
-            width: 100%;
-          }
-
-          .CountryList-links {
-            display: flex;
-            flex-direction: row;
-          }
-
-          .info-name {
-            font-size: 2em;
-          }
-
-          .info-text {
-            font-size: 1em;
-          }
-
           .graph-info {
             width: 100%;
-          }
-
-          .names {
-            display: flex;
-            flex-direction: row;
-            align-items: baseline;
-            height: 100%;
-          }
-          .name-section {
-            display: flex;
-            flex-direction: row;
-            align-items: baseline;
-          }
-
-          .journalist-name {
-            font-size: 2em;
-          }
-
-          .journalist-date {
-            font-size: 1em;
-          }
-
-          .journalist-location {
-            font-size: 1em;
-          }
-
-          .journalist-organization {
-            font-size: 1em;
           }
         }
 
--- components/graph/CountryInfo.tsx
+++ components/graph/CountryInfo.tsx
@@ -14,23 +14,73 @@ const CountryInfo: FunctionComponent<CountryInfoProps> = (
 
   const pluralOrSingular = numAttacks > 1 ? "journalists" : "journalist";
 
-  return country ? (
+  return (
     <div className="CountryInfo">
-      <p className="info-text">
-        <span className="info-name">{country}</span>
-        has {numAttacks} {pluralOrSingular} killed since 1992.
-      </p>
-    </div>
-  ) : (
-    <div className="CountryInfo">
-      <p className="default-text">
-        Select any of the countries on the left to see data. Then select any of
-        the journalists to see their biography. Data is sourced from the{" "}
-        <a target="_blank" rel="noopener noreferrer" href="https://cpj.org/">
-          Committee to Protect Journalists
-        </a>
-        .
-      </p>
+      {country ? (
+        <p className="info-text">
+          <span className="info-name">{country}</span>
+          has {numAttacks} {pluralOrSingular} killed since 1992.
+        </p>
+      ) : (
+        <p className="default-text">
+          Select any of the countries on the left to see data. Then select any
+          of the journalists to see their biography. Data is sourced from the{" "}
+          <a target="_blank" rel="noopener noreferrer" href="https://cpj.org/">
+            Committee to Protect Journalists
+          </a>
+          .
+        </p>
+      )}
+      <style jsx>
+        {`
+          .info-text {
+            margin: 0;
+            margin-left: 20px;
+            max-width: 960px;
+            font-size: 1.25em;
+            color: #747474;
+            font-weight: 700;
+          }
+
+          .info-name {
+            font-size: 2.5em;
+            color: #e10000;
+            font-weight: 400;
+            padding-right: 5px;
+          }
+
+          .default-text {
+            margin: 18px;
+            font-size: 1.25em;
+            color: #747474;
+            font-weight: 700;
+          }
+
+          .default-text a {
+            text-decoration: underline;
+            color: #747474;
+          }
+
+          .default-text a:visited {
+            text-decoration: none;
+            color: #747474;
+          }
+
+          .default-text a:hover {
+            color: black;
+          }
+
+          @media (max-width: 700px) {
+            .info-name {
+              font-size: 2em;
+            }
+
+            .info-text {
+              font-size: 1em;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
