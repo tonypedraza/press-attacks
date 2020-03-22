df21853a05cd35ea60c4f6938e4b638b57c8557d
Luis Pedraza <luispedraza@utexas.edu>
2020-03-22 14:41:35 -0400

    moved data fetch in index to getStaticProps()

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,75 +1,34 @@
-// import { Flex, Box } from "rebass";
-// import { PrimaryButton } from "@nice-boys/components";
-// import { GetViewerComponent } from "../graphql/queries/getViewer.generated";
-// import Heading from "../components/Heading";
-// import Home from "../components/Home";
-
-// export default () => {
-//   return (
-//     <GetViewerComponent>
-//       {({ data, loading, error }) => {
-//         if (data) {
-//           return (
-//             <Flex alignItems="center" justifyContent="center" mt={4}>
-//               {data.viewer && data.viewer.name ? (
-//                 <Flex flexDirection="column" alignItems="center">
-//                   <Heading>
-//                     👋 Hello, {data.viewer ? data.viewer.name : "anonymous"}!
-//                   </Heading>
-//                   <Box mt={2}>
-//                     <a href="/api/auth/logout">
-//                       <PrimaryButton>Log out</PrimaryButton>
-//                     </a>
-//                   </Box>
-//                 </Flex>
-//               ) : (
-//                 /* Implement an Error page here if
-//                 you want to enforce user accounts */
-//                 <Home />
-//               )}
-//             </Flex>
-//           );
-//         }
-
-//         if (loading) return <p>Loading...</p>;
-
-//         if (error) return <p>Error :(</p>;
-
-//         return null;
-//       }}
-//     </GetViewerComponent>
-//   );
-// };
-
 import React from "react";
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 import App from "../components/App";
-import { GetCountriesComponent } from "../graphql/queries/getCountries.generated";
+import { Country } from "../types/press-attacks";
 
-const Home: NextPage = () => {
-  return (
-    <GetCountriesComponent>
-      {({ data, loading, error }) => {
-        if (data && data.countries) {
-          return <App countries={data.countries} />;
-        }
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error :(</p>;
-        return null;
-      }}
-    </GetCountriesComponent>
-  );
+const Home: NextPage<{ countries: Country[] }> = ({ countries }) => (
+  <App countries={countries} />
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const query = `query getCountries {
+    countries(orderBy: name_ASC) {
+      id
+      name
+      numJournalists
+    }
+  }`;
+  const url = "http://localhost:3000/api/graphql";
+  const opts = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query })
+  };
+  const res = await fetch(url, opts);
+  const resJson = await res.json();
+  const countries = resJson.data.countries;
+  return {
+    props: {
+      countries
+    }
+  };
 };
 
 export default Home;
-
-{
-  /* const Home: NextPage<{ userAgent: string }> = () => <App />;
-
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
-  return { userAgent };
-};
-
-export default Home; */
-}
